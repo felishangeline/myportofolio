666b5aa0e2054af76dc93758e3cd80bc67553ebb
felisha.angeline <felishangeline@gmail.com>
2026-09-09 15:31:44 +0700

    menambahkan model skill dan menambahkan skill di nav bar

--- main/models.py
+++ main/models.py
@@ -29,6 +29,12 @@ class Experience(models.Model):
     def is_ongoing(self):
         return self.ended_at is None
 
+class Skills(models.Model):
+    COMMON_SKILLS = [
+        ('language', 'Language')
+    ]
 
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
     
